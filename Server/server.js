7f4a79329ce75446ed6c538d036ce2fd4f3b0e2d
Caden Parker <cadenparker@ymail.com>
2019-09-14 13:45:13 -0700

    Playlist Management

--- Server/server.js
+++ Server/server.js
@@ -23,7 +23,10 @@ const {songSchema, albumSchema, userSchema} = require('./models.js');
 let db;
 mongoClient.connect(
     'mongodb://localhost/',
-    {useNewUrlParser: true, useUnifiedTopology: true},
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
     (err, client) => {
         if (err) console.warn("Mongo isn't running"), process.exit();
         db = client.db('HiFi');
@@ -33,7 +36,9 @@ mongoClient.connect(
 // Login/Reg
 api.post(
     '/register',
-    multer({storage: multer.memoryStorage()}).none(),
+    multer({
+        storage: multer.memoryStorage(),
+    }).none(),
     async (request, response) => {
         let errors = userSchema.validate(request.body).error;
         if (errors != undefined) return response.status(400).json(errors);
@@ -46,38 +51,166 @@ api.post(
         };
         newUser.email = newUser.email.toLowerCase();
 
-        let emailDupe = await db
-            .collection('users')
-            .findOne({email: newUser.email});
+        let emailDupe = await db.collection('users').findOne({
+            email: newUser.email,
+        });
         if (emailDupe)
-            return response.status(400).json({msg: 'Duplicate Email'});
+            return response.status(400).json({
+                msg: 'Duplicate Email',
+            });
 
         let salt = await bcrypt.genSalt(12);
         let hashed_password = await bcrypt.hash(newUser.password, salt);
         newUser.password = hashed_password;
         await db.collection('users').insertOne(newUser);
 
-        return response.status(201).json({msg: 'Successful Login'});
+        return response.status(201).json({
+            msg: 'Successful Login',
+        });
     },
 );
 
 api.post(
     '/login',
-    multer({storage: multer.memoryStorage()}).none(),
+    multer({
+        storage: multer.memoryStorage(),
+    }).none(),
     async (request, response) => {
+        console.log(request.session.user);
         let errors = userSchema.validate(request.body).error;
         if (errors != undefined) return response.status(400).json(errors);
 
-        let userInDb = await db
-            .collection('users')
-            .findOne({email: request.body.email});
-        if (!userInDb) return response.json({msg: 'Incorrect Email'});
+        let userInDb = await db.collection('users').findOne({
+            email: request.body.email,
+        });
+        if (!userInDb)
+            return response.json({
+                msg: 'Incorrect Email',
+            });
 
         if (await bcrypt.compare(request.body.password, userInDb.password)) {
             request.session.user = userInDb._id;
-            response.json({msg: 'Login Successful'});
+            response.json({
+                msg: 'Login Successful',
+            });
         } else {
-            response.json({msg: 'Incorrect Password'});
+            response.json({
+                msg: 'Incorrect Password',
+            });
+        }
+    },
+);
+
+api.get('/logout', (request, response) => {
+    delete request.session.user;
+    response.json({
+        msg: 'Logout Successful',
+    });
+});
+
+// creating/managing playlists
+api.get('/playlist/new', async (request, response) => {
+    let data = {
+        name: 'New Playlist',
+        creatorID: request.session.user,
+        picture: '',
+        description: '',
+        songs: [],
+    };
+
+    let newPlaylist;
+    try {
+        newPlaylist = (await db.collection('playlists').insertOne(data)).ops[0];
+    } catch (err) {
+        return response.status(500).json(err);
+    }
+
+    try {
+        await db.collection('users').findOneAndUpdate(
+            {
+                _id: new ObjectID(request.session.user),
+            },
+            {
+                $push: {
+                    playlists: newPlaylist._id,
+                },
+            },
+        );
+        response.status(201).json(newPlaylist);
+    } catch (err) {
+        response.status(500).json(err);
+    }
+});
+
+api.get('/playlist/view/:playlistID', async (request, response) => {
+    try {
+        let playlist = await db.collection('playlists').findOne({
+            _id: new ObjectID(request.params.playlistID),
+        });
+        response.json(playlist);
+    } catch (err) {
+        response.status(400).json({msg: 'Invalid Playlist ID'});
+    }
+});
+
+api.post(
+    '/playlist/addsong/',
+    multer({storage: multer.memoryStorage()}).none(),
+    async (request, response) => {
+        let song, playlist;
+        try {
+            song = await db
+                .collection('songs')
+                .findOne({_id: new ObjectID(request.body.songID)});
+        } catch (err) {
+            response.status(400).json({msg: 'Invalid Song ID'});
+        }
+
+        try {
+            playlist = await db
+                .collection('playlists')
+                .findOneAndUpdate(
+                    {_id: new ObjectID(request.body.playlistID)},
+                    {$addToSet: {songs: new ObjectID(request.body.playlistID)}},
+                );
+            response.json({msg: 'Playlist Updated Successfully'});
+        } catch (err) {
+            response.status(400).json({msg: 'Invalid Playlist ID'});
+        }
+    },
+);
+
+api.post(
+    '/playlist/editmeta/:playlistID',
+    multer({storage: multer.memoryStorage()}).none(),
+    async (request, response) => {
+        let playlistID;
+        try {
+            playlistID = new ObjectID(request.params.playlistID);
+            let oldPlaylist = await db
+                .collection('playlists')
+                .findOne({_id: playlistID});
+        } catch (err) {
+            response.status(400).json({msg: 'Invalid Playlist ID'});
+        }
+        let editedPlaylist = {
+            $set: {
+                name: request.body.name ? request.body.name : oldPlaylist.name,
+            },
+            $set: {
+                description: request.body.description
+                    ? request.body.description
+                    : oldPlaylist.description,
+            },
+        };
+        try {
+            await db
+                .collection('playlist')
+                .findOneAndUpdate({_id: playlistID}, editedPlaylist);
+            response.json({msg: 'Meta Data Changed Successfully'});
+        } catch (err) {
+            console.log(err);
+            response.status(500).json({msg: 'Meta Data Change Failed'});
         }
     },
 );
@@ -85,15 +218,26 @@ api.post(
 // song upload/streaming/info
 api.post('/upload', (request, response) => {
     const storage = multer.memoryStorage();
-    const upload = multer({storage: storage, limits: {files: 3, parts: 8}});
+    const upload = multer({
+        storage: storage,
+        limits: {
+            files: 3,
+            parts: 8,
+        },
+    });
 
     upload.array('files', 3)(request, response, err => {
-        if (err) return response.status(400).json({msg: 'Invalid Upload'});
+        if (err)
+            return response.status(400).json({
+                msg: 'Invalid Upload',
+            });
         if (request.files.length != 3)
-            return response.status(400).json({msg: 'Invalid Upload'});
+            return response.status(400).json({
+                msg: 'Invalid Upload',
+            });
 
         let newSong = {
-            title: request.body.name,
+            title: request.body.title,
             artist: request.body.artist,
             duration: request.body.duration,
             trackNumber: request.body.trackNumber,
@@ -105,7 +249,10 @@ api.post('/upload', (request, response) => {
             albumID: request.body.albumID,
         };
         let schemaErrors = songSchema.validate(newSong).error;
-        if (schemaErrors) return response.status(400).json({msg: schemaErrors});
+        if (schemaErrors)
+            return response.status(400).json({
+                msg: schemaErrors,
+            });
 
         request.files.sort((a, b) => a.size > b.size); // sort least to greatest
 
@@ -132,7 +279,9 @@ api.post('/upload', (request, response) => {
             readable.pipe(uploadStream);
 
             uploadStream.on('error', () => {
-                return response.status(500).json({msg: 'Error Uploading File'});
+                return response.status(500).json({
+                    msg: 'Error Uploading File',
+                });
             });
             uploadStream.on('finish', () => {
                 successCount++;
@@ -149,9 +298,13 @@ api.post('/upload', (request, response) => {
                                 msg: 'Error adding song to database',
                             });
                         db.collection('albums').findOneAndUpdate(
-                            {_id: new ObjectID(res.ops[0].albumID)},
                             {
-                                $push: {songs: res.ops[0]._id},
+                                _id: new ObjectID(res.ops[0].albumID),
+                            },
+                            {
+                                $push: {
+                                    songs: res.ops[0]._id,
+                                },
                             },
                             (err, data) => {
                                 console.log(data);
@@ -170,12 +323,16 @@ api.get('/stream/:id', (request, response) => {
     try {
         DataID = new ObjectID(request.params.id);
     } catch (err) {
-        return response.status(400).json({msg: 'Invalid TrackId'});
+        return response.status(400).json({
+            msg: 'Invalid TrackId',
+        });
     }
     //    response.set('content-type', 'audio/mp4');
     response.set('accept-ranges', 'bytes');
 
-    let bucket = new mongodb.GridFSBucket(db, {bucketName: 'songs'});
+    let bucket = new mongodb.GridFSBucket(db, {
+        bucketName: 'songs',
+    });
 
     let downloadStream = bucket.openDownloadStream(DataID);
 
@@ -191,31 +348,50 @@ api.get('/song/:id', async (request, response) => {
     try {
         SongID = new ObjectID(request.params.id);
     } catch (err) {
-        return response.status(400).json({msg: 'Invalid Song Id'});
+        return response.status(400).json({
+            msg: 'Invalid Song Id',
+        });
     }
 
-    response.json(await db.collection('songs').findOne({_id: SongID}));
+    response.json(
+        await db.collection('songs').findOne({
+            _id: SongID,
+        }),
+    );
 });
 
-api.post('/create/album/', (request, response) => {
+api.post('/album/create', (request, response) => {
     const storage = multer.memoryStorage();
-    const upload = multer({storage: storage, limits: {files: 1, parts: 2}});
+    const upload = multer({
+        storage: storage,
+        limits: {
+            files: 1,
+            parts: 2,
+        },
+    });
 
     upload.single('image', 1)(request, response, err => {
-        if (err) return response.status(400).json({msg: 'Invalid Upload'});
+        if (err)
+            return response.status(400).json({
+                msg: 'Invalid Upload',
+            });
 
         const readable = new Readable();
         readable.push(request.file.buffer);
         readable.push(null);
 
-        let bucket = new mongodb.GridFSBucket(db, {bucketName: 'images'});
+        let bucket = new mongodb.GridFSBucket(db, {
+            bucketName: 'images',
+        });
 
         let uploadStream = bucket.openUploadStream(request.body.name);
         let artID = uploadStream.id;
         readable.pipe(uploadStream);
 
         uploadStream.on('error', () => {
-            return response.status(500).json({msg: 'Error Uploading File'});
+            return response.status(500).json({
+                msg: 'Error Uploading File',
+            });
         });
 
         uploadStream.on('finish', () => {
@@ -227,9 +403,9 @@ api.post('/create/album/', (request, response) => {
             console.log(albumSchema.validate(newAlbum));
             db.collection('albums').insertOne(newAlbum, (err, res) => {
                 if (err)
-                    return response
-                        .status(500)
-                        .json({msg: 'Error adding album to database'});
+                    return response.status(500).json({
+                        msg: 'Error adding album to database',
+                    });
                 return response.status(201).json(res.ops);
             });
         });
